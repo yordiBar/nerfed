a98fff656fea06e85e7b3d59e2eecb2b918a959d
Bartek Rybak <bart.rybak@hotmail.com>
2020-11-07 08:25:52 +0000

    embedded youtube

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" 
-    integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" 
-    crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/globals.css">
-    <link rel="stylesheet" href="css/style.css">    
+    <link rel="stylesheet" href="css/style.css">
     <title>Nerfed</title>
 </head>
+
 <body>
     <!-- Navbar-->
     <div class="navbar">
@@ -31,12 +31,38 @@
             <div>
                 <h1 class="xl">Videos</h1>
                 <p class="lead">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quam animi sapiente nam alias ullam fugit inventore 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quam animi sapiente nam alias ullam fugit inventore
                     provident repudiandae cumque beatae.
                 </p>
             </div>
             <img src="images/server.png" alt="">
+        </div>
+    </section>
+
+    <!-- Videos-main -->
+    <section class="videos-main my-2">
+        <div class="container grid grid-3">
+            <div class="card flex">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/8hASdMQFPBs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="card flex">
+                <iframe width="350" height="215" src="https://www.youtube.com/embed/bcTzt42y0nM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="card flex">
+            <iframe width="350" height="215" src="https://www.youtube.com/embed/Yip7CmKHOEk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="container grid grid-3">
+            <div class="card flex">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/vd_N6gYHpUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="card flex">
+            <iframe width="350" height="215" src="https://www.youtube.com/embed/xL7l_REw9sE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="card flex">
+            <iframe width="350" height="215" src="https://www.youtube.com/embed/L4Etc-ZGQEo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         </div>
     </section>
 
@@ -46,8 +72,8 @@
             <div>
                 <h1 class="md">Another Section Title</h1>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Quam animi sapiente nam alias ullam fugit inventore 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quam animi sapiente nam alias ullam fugit inventore
                     provident repudiandae cumque beatae.
                 </p>
             </div>
@@ -55,47 +81,6 @@
         </div>
     </section>
 
-    <!-- Videos-main -->
-    <section class="videos-main my-2">
-        <div class="container grid grid-3">
-            <div class="card flex">
-                <i class="fas fa-server fa-3x"></i>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-            </div>
-            <div class="card flex">
-                <i class="fas fa-server fa-3x"></i>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-            </div>
-            <div class="card flex">
-                <i class="fas fa-server fa-3x"></i>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-            </div>
-            <div class="card flex">
-                <i class="fas fa-server fa-3x"></i>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-            </div>
-            <div class="card flex">
-                <i class="fas fa-server fa-3x"></i>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-            </div>
-            <div class="card flex">
-                <i class="fas fa-server fa-3x"></i>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-            </div>
-        </div>
-    </section>
 
     <!-- Footer -->
     <footer class="footer bg-dark py-5">
@@ -120,4 +105,5 @@
         </div>
     </footer>
 </body>
+
 </html>
