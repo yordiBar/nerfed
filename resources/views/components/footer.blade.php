91198b7938649ff7bf8a2491a1ca77ff2394d68d
Bartek Rybak <bart.rybak@hotmail.com>
2020-11-07 20:11:30 +0000

    created footer and navbar files

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,24 @@
 <div>
-    <!-- Be present above all else. - Naval Ravikant -->
+    <!-- Footer -->
+    <footer class="footer bg-dark py-5">
+        <div class="container grid grid-3">
+            <div>
+                <h1>Nerfed</h1>
+                <p>Copyright &copy; 2020</p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/videos">Videos</a></li>
+                    <li><a href="/gallery">Gallery</a></li>
+                </ul>
+            </nav>
+            <div class="social">
+                <a href="#"><i class="fab fa-github fa-2x"></i></a>
+                <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
+                <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+                <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+            </div>
+        </div>
+    </footer>
 </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,15 @@
 <div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
+    <!-- Navbar-->
+    <div class="navbar">
+        <div class="container flex">
+            <h1 class="logo">Nerfed</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/videos">Videos</a></li>
+                    <li><a href="/gallery">Gallery</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </div>
