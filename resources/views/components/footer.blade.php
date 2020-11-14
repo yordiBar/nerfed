0ef4d0372403a50a41d46cd83c65b63707f3730e
Bartek Rybak <bart.rybak@hotmail.com>
2020-11-14 22:37:59 +0000

    added LinkedIn icon and link to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -16,8 +16,7 @@
                 <a href="https://www.youtube.com/channel/UC4EIFBatBM5pdB1tiRm1QPA/featured" target="_blank">
                     <i class="fab fa-youtube fa-2x"></i></a>
                 <a href="https://github.com/yordiBar" target="_blank"><i class="fab fa-github fa-2x"></i></a>
-                <a href="#" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
-                <a href="#" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+                <a href="https://www.linkedin.com/in/bart-rybak/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
             </div>
         </div>
     </footer>
